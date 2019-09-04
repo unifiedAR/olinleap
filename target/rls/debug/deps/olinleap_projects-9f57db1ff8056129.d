adae654fcb6612b68e8d80882b8ed3d817fb1fc8
Jonathan Kelley <jkelleyrtp@gmail.com>
2019-09-04 19:54:30 -0400

    Simple workspace added

--- target/rls/debug/deps/olinleap_projects-9f57db1ff8056129.d
+++
@@ -1,5 +0,0 @@
-/Users/jonkelley/Development/Projects/olinleap-rs/target/rls/debug/deps/olinleap_projects-9f57db1ff8056129.rmeta: olinleap-projects/src/lib.rs
-
-/Users/jonkelley/Development/Projects/olinleap-rs/target/rls/debug/deps/olinleap_projects-9f57db1ff8056129.d: olinleap-projects/src/lib.rs
-
-olinleap-projects/src/lib.rs:
--- target/rls/debug/deps/olinleap_projects-eda7de62c976f8d8.d
+++
@@ -1,5 +0,0 @@
-/Users/jonkelley/Development/Projects/olinleap-rs/target/rls/debug/deps/olinleap_projects-eda7de62c976f8d8.rmeta: olinleap-projects/src/lib.rs
-
-/Users/jonkelley/Development/Projects/olinleap-rs/target/rls/debug/deps/olinleap_projects-eda7de62c976f8d8.d: olinleap-projects/src/lib.rs
-
-olinleap-projects/src/lib.rs:
